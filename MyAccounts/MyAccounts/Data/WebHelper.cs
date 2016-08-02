5009eb1234eac6301375d86578ecb647d0097890
Kyle <engineer_03@webdesigns.com.tw>
2016-08-02 09:46:49 +0800

    減少view的邏輯故新增WebHelper來判斷收支類型，Child Action直接把值帶入helper即可取得類型

---
+++ MyAccounts/MyAccounts/Data/WebHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyAccounts.Data
+{
+    public static class WebHelper
+    {
+        public static string GetAccountType(string type)
+        {
+            return type == "1" ? "支出" : "收入";
+        }
+    }
+}
